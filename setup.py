dbf1ced7dce267962914203e90803200e45bee8d
floptical <stephen.macdavid@gmail.com>
2021-02-09 11:27:12 -0500

    testing out upgrading dependencies

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name='S3 SFTP Sync',
-    version='0.1dev',
+    version='0.2dev',
     packages=['s3_sftp_sync',],
     description='Syncs files from a SFTP server to an S3 bucket',
     long_description=open('README.md').read(),
     install_requires=[
-        'boto3==1.4.4',
-        'click==6.7',
-        'PyYAML==3.12',
+        'boto3==1.17.3',
+        'click==7.1.2',
+        'PyYAML==5.4.1',
         'pysftp==0.2.9'
     ],
     entry_points={
