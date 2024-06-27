18d1f4dda112655ea5fbdd2812cc3c57de24f9cf
floptical <stephen.macdavid@gmail.com>
2024-06-27 17:14:28 +0000

    initial start of work, non-functional

--- s3_sftp_sync/cli.py
+++ s3_sftp_sync/cli.py
@@ -2,29 +2,18 @@ import sys
 import os
 import datetime
 import hashlib
-import logging
-from logging.config import dictConfig
+
 
 import yaml
-import pysftp
-from pysftp.helpers import WTCallbacks
 import click
 import boto3
 import botocore
 
-logger = None
+import os
+import paramiko
+from stat import S_ISDIR
 
-def get_logger(logging_config):
-    try:
-        with open(logging_config) as file:
-            config = yaml.load(file)
-        dictConfig(config)
-    except Exception as e:
-        FORMAT = '[%(asctime)-15s] %(levelname)s [%(name)s] %(message)s'
-        logging.basicConfig(format=FORMAT, level=logging.INFO)
-        #logging.error(str(e))
 
-    return logging.getLogger('s3_sftp_sync')
 
 def get_config(config_file):
     print(f'trying to open {config_file}')
@@ -83,14 +72,28 @@ def s3_md5(s3, bucket, key):
 
     return None
 
+def list_files_recursively(sftp, directory):
+    print('listing all files..')
+    all_files = []
+
+    def _list_files(path):
+        files = sftp.listdir_attr(path)
+        for file in files:
+            file_path = os.path.join(path, file.filename)
+            if stat.S_ISDIR(file.st_mode):
+                _list_files(file_path)
+            else:
+                all_files.append(file_path)
+
+    _list_files(directory)
+    return all_files
+
+
 @click.command()
 @click.option('--config-file', default='./config.conf')
-@click.option('--logging-config', default='./logging_config.conf')
-def main(config_file, logging_config):
-    global logger
+def main(config_file):
 
 
-    logger = get_logger(logging_config)
     config = get_config(config_file)
 
     print('Starting sync')
@@ -122,64 +125,68 @@ def main(config_file, logging_config):
                 print('Could not fetch last modified time S3 object - {}/{}'.format(bucket, key))
                 sys.exit(1)
 
-    cnopts = pysftp.CnOpts()
-    cnopts.compression = True
-    cnopts.hostkeys = None
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    # Connect to the server
+    print(f"Connecting to {config['sftp']['hostname']}..")
+    client.connect(hostname=config['sftp']['hostname'], username=config['sftp']['username'], password=config['sftp']['password'], port=22,timeout=5)
+    transport = client.get_transport()
+    transport.set_keepalive(30)
+    print('connected.')
 
-    with pysftp.Connection(config['sftp']['hostname'],
-                           username=config['sftp']['username'],
-                           password=config['sftp']['password'],
-                           cnopts=cnopts) as sftp:
+    try:
+        with client.open_sftp() as sftp:
+            print('hi')
+            all_files = list_files_recursively(sftp, ".")
+            
+            for file in all_files:
+                print(file)
+            raise
+            for fname in wtcb.flist:
+                stats = sftp.sftp_client.stat(fname)
 
-        print('Walking SFTP server structure')
-        wtcb = WTCallbacks()
-        # https://pysftp.readthedocs.io/en/release_0.2.8/pysftp.html#pysftp.Connection.walktree
-        # 1st arg is the root of the remote directory to descend the cwd
-        # 2nd arg is fcallback, "callback  function  to  invoke  for  a  regular  file."
-        # 3rd arg is dcallback, "callback  function  to  invoke  for  a  directory."
-        # 4th arg is ucallback, " callback function to invoke for an unknown file type."
-        # 5th arg is a boolean for recursing (default should be true)
-        sftp.walktree('.', wtcb.file_cb, wtcb.dir_cb, wtcb.unk_cb, True)
+                mtime = str(stats.st_mtime)
+                size = stats.st_size
+                if start_time == None or mtime >= start_time:
+                    with sftp.sftp_client.file(fname) as file:
+                        if mtime == start_time:
+                            s3_hash = s3_md5(s3, bucket, key_prefix + fname)
 
-        for fname in wtcb.flist:
-            stats = sftp.sftp_client.stat(fname)
+                            # if s3 object doesn't exist, don't bother hashing sftp file
+                            if s3_hash != None:
+                                print('{} modified time equals start_time, hash checking file'.format(fname))
+                                file_hash = file_md5(file)
+                            else:
+                                file_hash = None
+                        if start_time == None or mtime > start_time or s3_hash != file_hash:
+                            print('Syncing {} - {} mtime - {} bytes'.format(fname, mtime, size))
 
-            mtime = str(stats.st_mtime)
-            size = stats.st_size
-            if start_time == None or mtime >= start_time:
-                with sftp.sftp_client.file(fname) as file:
-                    if mtime == start_time:
-                        s3_hash = s3_md5(s3, bucket, key_prefix + fname)
+                            s3.put_object(
+                                Bucket=bucket,
+                                Key=key_prefix + fname,
+                                Body=file,
+                                Metadata={
+                                    'sftp_mtime': mtime,
+                                    'sftp_sync_time': datetime.datetime.utcnow().isoformat()
+                                })
 
-                        # if s3 object doesn't exist, don't bother hashing sftp file
-                        if s3_hash != None:
-                            print('{} modified time equals start_time, hash checking file'.format(fname))
-                            file_hash = file_md5(file)
-                        else:
-                            file_hash = None
-                    if start_time == None or mtime > start_time or s3_hash != file_hash:
-                        print('Syncing {} - {} mtime - {} bytes'.format(fname, mtime, size))
+                            num_files_synced += 1
+                            num_bytes_synced += size
 
-                        s3.put_object(
-                            Bucket=bucket,
-                            Key=key_prefix + fname,
-                            Body=file,
-                            Metadata={
-                                'sftp_mtime': mtime,
-                                'sftp_sync_time': datetime.datetime.utcnow().isoformat()
-                            })
+                if 'incremental_sync' in config and (last_modified == None or mtime >= last_modified):
+                    last_modified = mtime
 
-                        num_files_synced += 1
-                        num_bytes_synced += size
+            if 'incremental_sync' in config and last_modified != None and last_modified != start_time:
+                print('Updating last_modified time {}'.format(last_modified))
+                s3.put_object(
+                    Bucket=bucket,
+                    Key=config['incremental_sync']['last_modified_s3_key'],
+                    Body=str(last_modified).encode('utf8'))
 
-            if 'incremental_sync' in config and (last_modified == None or mtime >= last_modified):
-                last_modified = mtime
-
-        if 'incremental_sync' in config and last_modified != None and last_modified != start_time:
-            print('Updating last_modified time {}'.format(last_modified))
-            s3.put_object(
-                Bucket=bucket,
-                Key=config['incremental_sync']['last_modified_s3_key'],
-                Body=str(last_modified).encode('utf8'))
-
-        print('Synced {} files and {} bytes'.format(num_files_synced, num_bytes_synced))
+            print('Synced {} files and {} bytes'.format(num_files_synced, num_bytes_synced))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Close the SSH client
+        client.close()
